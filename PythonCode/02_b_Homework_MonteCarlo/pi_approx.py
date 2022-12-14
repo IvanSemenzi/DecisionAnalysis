ac54b89398eda58abf0998877d18ac2c0506ac08
tommasoguidi <tommaso.guidi.1998@gmail.com>
2022-12-14 18:52:30 +0100

    aggiunto codice originale in calce

--- PythonCode/02_b_Homework_MonteCarlo/pi_approx.py
+++ PythonCode/02_b_Homework_MonteCarlo/pi_approx.py
@@ -1,3 +1,66 @@
+""" ORIGINAL MATLAB CODE
+%% Clean workspace
+
+close all;
+clear all;
+clc;
+
+
+
+%% Parameters
+% number of points in the square
+Ns=1e3;
+
+
+
+%% Simulate points
+
+% generate Ns points uniformly distributed in the square
+x=2*rand(Ns,1)-1;
+y=2*rand(Ns,1)-1;
+
+
+
+%% Compute and display results
+
+% distance from the origin of all the points
+d=sqrt(x.^2+y.^2);
+
+% index and number of points in the circle
+Ic=(d<=1);
+Nc=sum(Ic);
+
+% approximation of pi
+pihat=4*Nc/Ns;
+
+disp(['Number of points in the square: ' num2str(Ns)])
+disp(['            "True" value of pi: ' num2str(pi)])
+disp(['      Approximated value of pi: ' num2str(pihat)])
+
+% Just a figure...
+figure,
+hold on
+% Square
+square_x=[-1 -1 1  1 -1];
+square_y=[-1  1 1 -1 -1];
+plot(square_x,square_y,'b','LineWidth',2);
+
+% Circle
+theta=[-pi:0.01:pi];
+circle_x=cos(theta);
+circle_y=sin(theta);
+plot(circle_x,circle_y,'r','LineWidth',2);
+
+% Points
+plot(x,y,'ob');
+plot(x(Ic),y(Ic),'xr');
+
+% Axis
+axis([-1.1 1.1 -1.1 1.1])
+axis equal
+title(['N_s = ' num2str(Ns) ', N_c = ' num2str(Nc) ', (4\rho = ' num2str(pihat) ')'] )
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -28,11 +91,11 @@ ax.set_aspect(1)
 
 # for cycle to count how many points are inside the unit circle and draw them
 for i in range(N_s):
-    if((np.sqrt(points[i, 0]**2 + points[i, 1]**2)) <= 1):
-        plt.scatter(points[i,0], points[i, 1], color = 'b')
+    if np.sqrt(points[i, 0]**2 + points[i, 1]**2) <= 1:
+        plt.scatter(points[i,0], points[i, 1], color='b')
         N_c += 1
     else:
-        plt.scatter(points[i,0], points[i, 1], color = 'r')
+        plt.scatter(points[i,0], points[i, 1], color='r')
 
 pi_est = 4*N_c/N_s
 
