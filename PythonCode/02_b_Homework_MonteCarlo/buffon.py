7ff169ff2cea424ab21fd603ac1952e8100ee5f1
tommasoguidi <tommaso.guidi.1998@gmail.com>
2022-12-15 14:44:10 +0100

    buffon exercise

--- PythonCode/02_b_Homework_MonteCarlo/buffon.py
+++ PythonCode/02_b_Homework_MonteCarlo/buffon.py
@@ -1,73 +1,18 @@
-""" ORIGINAL MATLAB CODE
-%% Clean workspace
+import numpy as np
+import math
 
-close all;
-clear all;
-clc;
+N = [100, 1_000, 10_000, 1_000_000]                         # different number of simulations
 
+for i in N:
+    y0 = np.random.rand(i)                                  # y coordinate of the bottom left edge of the needle
+    theta = math.pi * (2 * np.random.rand(i) - 1)           # angle of the needle
 
+    y1 = y0 + np.cos(theta)                                 # y coordinate of the other edge of the needle
 
-%% Parameters
-% number of throws
-N=1e4;
+    crosses = np.sum(abs(np.floor(y0) - np.floor(y1)))      # number of needle crossing the line
 
-% 1D problem:
-%   needle position in [0, 1] (one extremum of the needle)
-%   needle angle in [-pi, pi]
-%   lines at position ..., 0, 1,...
+    p_hat = float(crosses/i)
 
+    print(f'The estimate of p after {i} experiments is : {p_hat}')
 
-
-%% Simulate random throws
-
-% simulate N positions and N angles
-x=rand(N,1);
-theta=pi*(2*rand(N,1)-1);
-
-
-
-%% Check intersection:
-%    "is there is a line between the two extrema of the needle?"
-%
-% one extremum is at x
-%    the other is at x+cos(theta);
-ext=x+cos(theta);
-
-% counter of the simulation runs where the needle crosses a line
-cross=0;
-
-for k=1:N
-
-        % intersection if: ext(k)>=1 or ext(k) <= 0
-        if ext(k)>= 1 | ext(k)<=0
-            cross=cross+1;
-        end
-
-end
-
-
-
-%% Compute and display results
-% true probability
-p=2/pi;
-
-% estimated probability
-phat=cross/N;
-
-disp(['         Number of throws: ' num2str(N)])
-disp(['         True probability: ' num2str(p)])
-disp([' Approximated probability: ' num2str(phat)])
-
-
-
-%% NOTE TO ADVANCED USERS
-%
-% If you exploit the Matlab vector capability, you can replace
-% the whole for loop by the following line (just one line!)
-
-%cross2=sum(ext>=1 | ext<=0);
-
-% Check it out that cross2 = cross
-%
-%cross2-cross
-"""
+print(f'The actual value of p is {2/math.pi}')
