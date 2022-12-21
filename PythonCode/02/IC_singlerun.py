016aebc0ae068cce0031df3a0486420dc14107f5
tommasoguidi <tommaso.guidi.1998@gmail.com>
2022-12-21 16:27:43 +0100

    monte carlo

--- PythonCode/02/IC_singlerun.py
+++ PythonCode/02/IC_singlerun.py
@@ -1,13 +1,15 @@
 """
 The function takes the time horizon T_max and the max capacity C_max
 
-returns the simulated states x, orders u, costs gt and demands W_real
+returns the simulated states x, orders u, stage costs gt and demands W_real
+DATA ARE RETURNED AS COLUMN VECTORS
 """
 
 import numpy as np
 
-def IC_singlerun(T_max:int, C_max:int) -> (int, int, int, int):
+def IC_singlerun(T_max:int) -> (int, int, int, int):
 
+    C_max = 10                                          # max capacity of the storage
     W_max = 4                                           # max demand
     pW = np.array([0.2, 0.2, 0.3, 0.2, 0.1])            # vector of  probabilities
     x_min = 4                                           # minimum number of elements in stock before reordering
@@ -15,11 +17,11 @@ def IC_singlerun(T_max:int, C_max:int) -> (int, int, int, int):
     c2 = 2                                              # unitary stage cost
 
     # preallocating vectors
-    x = np.zeros(T_max + 1)                             # state (elements in stock)
-    gt = np.zeros(T_max)                                # stage cost
-    u = np.zeros(T_max)                                 # input (elements ordered)
+    x = np.zeros(T_max + 1, dtype=int)                  # state (elements in stock)
+    gt = np.zeros(T_max, dtype=int)                     # stage cost
+    u = np.zeros(T_max, dtype=int)                      # input (elements ordered)
 
-    W_real = np.random.choice(np.arange(W_max + 1), T_max, replace=True, p=pW)  # realizations of demand
+    W_real = np.random.choice(np.arange(W_max + 1, dtype=int), T_max, replace=True, p=pW)  # realizations of demand
 
     x[0] = C_max                                        # initial condition
 
@@ -32,4 +34,5 @@ def IC_singlerun(T_max:int, C_max:int) -> (int, int, int, int):
             gt[t] = c2*x[t]
         x[t+1] = x[t] + u[t] - w                        # state update
 
-    return x, u, gt, W_real
+    # returned as row vectors
+    return x.reshape((-1, 1)), u.reshape((-1, 1)), gt.reshape((-1, 1)), W_real.reshape((-1, 1))
--- PythonCode/02/InventoryControl.py
+++ PythonCode/02/InventoryControl.py
@@ -1,14 +1,31 @@
 import numpy as np
 from IC_singlerun import IC_singlerun
+import matplotlib.pyplot as plt
 
 T_max = 100                                     # time horizon
-C_max = 10                                      # maximum capacity
 
-x, u, gt, W_real = IC_singlerun(T_max, C_max)   # runs a simulation over time horizon
+x, u, gt, W_real = IC_singlerun(T_max)   # runs a single simulation over time horizon
 
 print(f'Time horizon T: {T_max}')
 print()
 print(f'Total cost : {int(np.sum(gt))}')
-print(f'First reorder time: {next((i for i, x in enumerate(u) if x), None)}')
+print(f'First reorder time: {next((i for i, x in enumerate(u[0]) if x), None)}')
 print(f'Total number of orders: {int(np.sum(u))}')
 
+# plot
+fig, axs = plt.subplots(2, 2)
+fig.suptitle('Simulation')
+
+axs[0, 0].stairs(x[:, 0], range(T_max + 2))
+axs[0, 0].set_title('State (stock level)')
+
+axs[0, 1].bar(range(T_max), u[:, 0])
+axs[0, 1].set_title('Input (ordered quantity)')
+
+axs[1, 0].bar(range(T_max), gt[:, 0])
+axs[1, 0].set_title('Stage cost')
+
+axs[1, 1].fill_between(range(T_max), np.cumsum(gt[:, 0]))
+axs[1, 1].set_title('Total cost')
+
+plt.show()
