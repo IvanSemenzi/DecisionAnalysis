686da0c6e71aff7c75e1cc5a4e8e793fc231a696
tommasoguidi <tommaso.guidi.1998@gmail.com>
2023-01-16 18:13:32 +0100

    non so se va bene ma gira

---
+++ PythonCode/05_b/DP_appliance.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+
+
+# parameters and initialization
+# State x represents the NEXT cycle to run
+# possible states x = 0,1,...,N_state-1(=5)
+#                 x = 0         --> program not started yet
+#                 x = N_state-1 --> program completed
+
+# 5 washing cycles per program + program complete
+N_state = 6
+X_set = np.arange(N_state)
+
+# possible inputs: 0 = 'wait', 1 = 'run next cycle'
+N_input = 2
+U_set = np.array([0, 1], dtype=int)
+
+# time horizon t = 0,1,...,T
+T = 20      # sampling time = 15 min (5 hours time horizon)
+
+# power consumption profile (power required by each washing cycle, [kW])
+power = np.array([1, 2.5, 0.25, 0.25, 1.5, 0])
+
+# generate random energy prices
+np.random.seed(123)     # for reproducibility
+# contains the energy price at each time instant (in EUR/kWh)
+price = 0.2 + 0.05 * np.random.randn(T)
+
+# initialize matrices to store the results
+# U(x,t) is the optimal input if at time t we are in state x
+U = np.zeros((N_state, T), dtype=int)
+
+# optimal value functions (cost-to-go functions)
+# V(x,t) cost-to-go if at time t we are in state x
+V = np.zeros((N_state, T+1))
+
+# compute the optimal policy
+# DP algorithm START
+# 1. Init
+for k in range(N_state):
+    V[k, T] = DP_appliance_terminal_cost(X_set[k], N_state)
+
+# 2. Main Loop
+for t in range(T-1, -1, -1):
+    for s in X_set:
+        # s is the current state
+        if s == N_state - 1:
+            # if program completes, then u must be always 'wait' (0)
+            # -> u_star = 0
+            u_star = 0
+            # if program complete, cost-to-go is 0
+            V_star = 0
+        else:
+            # evaluate cost for all possible inputs
+            C = np.zeros(N_input)
+            for u in U_set:
+                # u is the current input
+                # next state if in state s we apply u
+                x_next = DP_appliance_f(s, u)
+                # total cost if at time t, state s, apply u
+                C[u] = DP_appliance_stage_cost(s, u, t, price, power) + V[x_next, t+1]
+            # find best input (u_star) and new cost-to-go value (V_star)
+            # C is a row vector containing the costs associated to each input (0,1)
+            u_star = np.argmin(C)
+            V_star = C[u_star]
+        # optimal input if in state x[k] at time t
+        U[s, t] = u_star
+        # cost-to-go if in state x[k] at time t
+        V[s, t] = V_star
+
+# simulate optimal policy
+# initialize state trajectory
+XX = np.zeros(T+1, dtype=int)
+# initialize total cost
+J = 0
+
+for t in range(T):
+    # compute next state using optimal policy stored in matrix U
+    # input to apply at time t is U[XX[0, t], t]
+    XX[t + 1] = DP_appliance_f(XX[t], U[XX[t], t])
+    # update total cost
+    J += DP_appliance_stage_cost(XX[t], U[XX[t], t], t, price, power)
+# add terminal cost
+J += DP_appliance_terminal_cost(XX[T], N_state)
+
+# check total cost
+print(f'Total cost with optimal policy (simulation): {J}')
+print(f'Total cost with optimal policy (cost-to-go function): {V[XX[0], 0]}')
+
+# plot results
+DP_appliance_plot_results(U, XX, price, power)
---
+++ PythonCode/05_b/utils.py
@@ -0,0 +1,118 @@
+"""
+This file contains the python code for all the matlab functions
+related to this exercise:
+
+            DP_appliance_f.m
+            DP_appliance_plot_results.m
+            DP_appliance_stage_cost.m
+            DP_appliance_terminal_cost.m
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def DP_appliance_f(x: int, u: int) -> int:
+    """
+
+    :param x:       current state
+    :param u:       current input
+    :return:        next state
+
+    input u can be:
+        u=0 wait
+        u=1 run next washing cycle
+    """
+    return x+u
+
+
+def DP_appliance_plot_results(U: np.ndarray,
+                              XX: np.ndarray,
+                              price: np.ndarray,
+                              power: np.ndarray):
+    """
+
+    :param U:       contains the optimal policy
+    :param XX:      contains the state trajectory
+    :param price:   contains the energy price at each time step
+    :param power:   contains the power consumption of each cycle
+    """
+    # display optimal policy
+    # each row is a state, each column is a time instant
+    # N_state is #states, T is #time_instants
+    N_state, T = U.shape
+
+    # visualize matrix U
+    fig, ax = plt.subplots()
+    fig.suptitle(r'Optimal policy $\mu_t(s)$: 0 wait (purple), 1 run (yellow)', fontsize=13)
+    ax.set(xlabel='Time [15min]', ylabel='State')
+    plt.pcolor(U)
+    plt.show()
+
+    # compare with naive policy
+    # optimal policy: find when we run, i.e. when the state changes
+    # UU(t) = 0 wait at time t
+    # UU(t) = 1 run at time t
+    UU = np.diff(XX)
+
+    # naive policy: run the 5 cycles during the 5 cheapest time slots
+    sorted_index = np.argsort(price)        # returns the index in the order that sorts the array (ascending)
+
+    # cost comparison
+    naive_cost = np.dot(price[np.sort(sorted_index[: N_state - 1])], power[: N_state - 1]) / 4
+    opt_cost = np.dot(price[np.nonzero(UU)], power[: N_state - 1]) / 4
+
+    # relative gain
+    rel_gain = (naive_cost - opt_cost)/naive_cost * 100
+
+    # compare with naive policy: Display when we run
+    fig, ax = plt.subplots()
+    fig.suptitle(f'Naive cost = {naive_cost}\n Optimal cost = {opt_cost}\n Relative gain = {round(rel_gain, 2)}%', fontsize=13)
+    ax.set(xlabel='Time [15min]', ylabel='Energy price [EUR/kWh')
+    # plot the prices at each time slot
+    plt.bar(np.arange(len(price)), price, align='edge', label='Energy prices')
+    # plot when we run according to naive policy (green slots)
+    plt.bar(sorted_index[: N_state - 1], price[sorted_index[: N_state - 1]], align='edge', color='green', label='Naive policy')
+    # plot when we run according to optimal policy (red slots)
+    plt.bar(np.arange(len(UU)), 0.02 * UU, align='edge', color='red', label='Optimal policy')
+    ax.legend()
+    plt.show()
+
+
+def DP_appliance_stage_cost(x: int, u: int, t: int,
+                            price: np.ndarray, power: np.ndarray) -> int:
+    """
+
+    :param x:           current state
+    :param u:           current input
+    :param t:           current time
+    :param price:       energy price vector
+    :param power:       power consumption profile
+    :return:            stage cost at time t
+
+    input u can be:
+        u=0 wait
+        u=1 run next washing cycle
+    """
+    g = (price[t] * power[x] * u) / 4
+
+    # N.B. Each washing cycle lasts 1 / 4 hour, price(t) is in EUR / kWh
+    #      power(x) is in kW. So, 1 / 4 * power(x) is the amount of
+    #      energy required by x-th washing cycle( in kWh)
+
+    return g
+
+
+def DP_appliance_terminal_cost(xT: int, N_state: int)-> int:
+    """
+
+    :param xT:          terminal state at final time T
+    :param N_state:     objective state to reach within T
+    :return:            terminal cost is set to inf to force the system to reach the target state
+    """
+
+    if xT == N_state - 1:
+        gT = 0              # program completed
+    else:
+        gT = 1e+6           # theoretically should be +inf, set to this to avoid errors
+
+    return gT
